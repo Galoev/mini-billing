b51430e2e6f84224dc85c90f5568aff84532ebf5
Galoev <galoyev.ilkin@gmail.com>
2020-12-15 04:34:26 +0300

    Реализовал класс Menu и вспомогательный класс Option

--- src/Billing.WebApi.Console/Menu.cs
+++ src/Billing.WebApi.Console/Menu.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Billing.WebApi.Console
 {
     public class Menu
     {
+        private IList<Option> options;
+
         public Menu()
         {
+            options = new List<Option>();
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < options.Count; i++)
+            {
+                System.Console.WriteLine($"{i + 1}. {options[i].name}");
+            }
+        }
+
+        public Menu Add(string option, Action callback)
+        {
+
+            return Add(new Option(option, callback));
+        }
+
+        private Menu Add(Option option)
+        {
+            options.Add(option);
+            return this;
         }
     }
 }
---
+++ src/Billing.WebApi.Console/Option.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Billing.WebApi.Console
+{
+    public class Option
+    {
+        public string name;
+        public Action callback;
+
+        public Option(string name, Action callback)
+        {
+            this.name = name;
+            this.callback = callback;
+        }
+    }
+}
