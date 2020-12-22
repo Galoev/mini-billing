f1d117684be7b2ed6e97d1bd389dfa36db7aff9e
zhvkgj <zhvkgj@yandex.ru>
2020-12-22 11:20:28 +0300

    Убрать из dto товара в заказе информацию об единице измерения, чтобы не возникало несоответствий с актуальной единицей измерения товара

--- src/Billing.WebApi.Client/Models/OrderGoodDto.cs
+++ src/Billing.WebApi.Client/Models/OrderGoodDto.cs
@@ -5,7 +5,6 @@ namespace Billing.WebApi.Client.Models
     public class OrderGoodDto
     {
         public Guid Id { get; set; }
-        public QuantityType QuantityType { get; set; }
         public int Quantity { get; set; }
     }
 }
