ef0bc52bce2fa59aaf9a5f9c1936be94c3c43912
Galoev <galoyev.ilkin@gmail.com>
2020-12-23 14:12:54 +0300

    Добавил модельки для заказов

--- src/Billing.WebApi.Console/Models/CreateOrder.cs
+++ src/Billing.WebApi.Console/Models/CreateOrder.cs
@@ -3,15 +3,12 @@ using System.Collections.Generic;
 
 namespace Billing.WebApi.Console.Models
 {
-    public class Order
+    public class CreateOrder
     {
         public Customer Customer { get; set; }
-
         public DateTime OrderDate { get; set; }
-
-        public decimal Price { get; set; }
         public PaymentStatus PaymentStatus { get; set; }
         public DeliveryStatus DeliveryStatus { get; set; }
-        public List<Good> Goods { get; set; }
+        public List<OrderGood> Goods { get; set; }
     }
 }
--- src/Billing.WebApi.Console/Models/InfoOrder.cs
+++ src/Billing.WebApi.Console/Models/InfoOrder.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-
 namespace Billing.WebApi.Console.Models
 {
-    public class Order
+    public class InfoOrder
     {
         public Customer Customer { get; set; }
-
         public DateTime OrderDate { get; set; }
-
         public decimal Price { get; set; }
         public PaymentStatus PaymentStatus { get; set; }
         public DeliveryStatus DeliveryStatus { get; set; }
-        public List<Good> Goods { get; set; }
     }
 }
