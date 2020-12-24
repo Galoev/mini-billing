19845474d78df6e2f64e283b45bf993794805dd4
zhvkgj <zhvkgj@yandex.ru>
2020-12-24 12:35:34 +0300

    Перенес цену компонента в модель компонента

--- src/Billing.WebApi/Models/Component.cs
+++ src/Billing.WebApi/Models/Component.cs
@@ -6,6 +6,7 @@ namespace Billing.WebApi.Models
     public class Component
     {
         public Guid Id { get; set; }
+        public decimal UnitPrice { get; set; }
         public QuantityType QuantityType { get; set; }
         public string Description { get; set; }
     }
--- src/Billing.WebApi/Models/GoodComponent.cs
+++ src/Billing.WebApi/Models/GoodComponent.cs
@@ -5,7 +5,6 @@ namespace Billing.WebApi.Models
     public class GoodComponent
     {
         public Guid Id { get; set; }
-        public decimal UnitPrice { get; set; }
         public int Quantity { get; set; }
     }
 }
